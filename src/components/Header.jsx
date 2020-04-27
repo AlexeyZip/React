babea37fa0cd838b715b499a997ce1d617aa5cf1
AlexeyZip <alexzipzap@gmail.com>
2020-04-27 20:17:08 +0300

    lesson13

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='Header'>
+        <img className='logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/600px-DC_Comics_logo.png' />
+    </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+  <div><img className='fon' src='https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'/></div>
+  <div><img className='ava' src='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'/></div>
+  <div>
+    My posts
+    <div>
+      new posts
+    </div>
+  </div>
+  <div>
+  <div>post1</div>          
+  <div>post2</div>          
+  <div>post3</div>          
+  </div>
+ </div>
+
+}
+export default Profile
