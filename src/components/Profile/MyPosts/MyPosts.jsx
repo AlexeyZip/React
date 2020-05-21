840fdbce7a1e9add95d97cdc278c6d9098c16f9b
AlexeyZip <alexzipzap@gmail.com>
2020-05-21 13:16:33 +0300

    lesson 16

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import s from './MyPosts.module.css'
 import Post from './Post/Post';
-const MyPosts = () => {
+const MyPosts = (props) => {
+  debugger;
   return <div>
       My posts
     <div>
-        new posts
+        {/* new posts */}
+        <textarea></textarea>
+        <button>Add post</button>
+        <button>Remove</button>
     </div>
       <div className={s.posts}>
-        <Post/>
-        <Post/>
+        <Post message='Hi, how are you?' amount = "2"/>
+        <Post message="It's my first post" amount = "3"/>
       </div>
     </div>
 
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import s from './Post.module.css'
-const Post = () => {
+const Post = (props) => {
+
+  console.log(props.message);
+
   return (
 
     <div className={s.item}>
       <img src="https://i2.wp.com/andrey-eltsov.ru/wp-content/uploads/2017/09/DsSd-Stim_hfhdY_jf-jfY-%D0%A5%D0%B0%D1%81%D0%BA%D0%B8.jpg" alt=""/>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae temporibus dignissimos sit rerum voluptates accusantium voluptate iure quae? Architecto earum nihil corrupti rem cupiditate deleniti illo recusandae, consectetur quas est.
+      {props.message}
       <div>
-        <span>like</span>
+  <span>like{props.amount}</span>
       </div>
     </div>
-  )
+  ) 
 }
+
 export default Post
