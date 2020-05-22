fdaf323168231c5889683fe2a73fc5033e5d2f96
AlexeyZip <alexzipzap@gmail.com>
2020-05-22 15:19:58 +0300

    lesson 22

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import { NavLink } from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div className={s.item}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/1">Kostya</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/2">Nastya</NavLink>
+            </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/3">Leha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/4">Roma</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/5">Andrey</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/6">Natasha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/7"> Galya</NavLink>
+                </div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Tell me why?</div>
+            </div>
         </div>
     )
 
