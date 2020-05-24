1267477dbd1ad48c5f8aef101af62c89e466f8a4
AlexeyZip <alexzipzap@gmail.com>
2020-05-24 19:50:09 +0300

    lesson 33

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,4 @@
+import { rerenderEntireTree } from "../render";
 
 let state = {
 
@@ -53,7 +54,7 @@ export let addPost = (postMessage) => {
     };
 
     state.profilePage.posts.push(newPost);
-
+    rerenderEntireTree(state);
 } 
 
 export default state;
