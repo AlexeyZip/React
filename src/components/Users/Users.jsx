4c046503e1e5c674b87eae9fa5a3590d1b86de35
AlexeyZip <alexzipzap@gmail.com>
2020-06-06 19:23:57 +0300

    lesson 53

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,39 +1,44 @@
 import React from 'react';
 import styles from './Users.module.css';
-
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-            { id: 1, photoUrl: 'https://bm.img.com.ua/nxs/img/prikol/images/large/1/2/308321_879389.jpg', followed: false, fullName: 'Alexey', status: 'I am a front-end Reac.js developer', location: { city: 'Charkov', country: 'Ukraine' } },
-            { id: 2, photoUrl: 'https://bm.img.com.ua/nxs/img/prikol/images/large/1/2/308321_879389.jpg', followed: true, fullName: 'Jaryck', status: 'I am a medic', location: { city: 'Pinsk', country: 'Belarus' } },
-            { id: 3, photoUrl: 'https://bm.img.com.ua/nxs/img/prikol/images/large/1/2/308321_879389.jpg', followed: false, fullName: 'Kostick', status: 'I am a good boy', location: { city: 'Charkov', country: 'Ukraine' } },
-            { id: 4, photoUrl: 'https://bm.img.com.ua/nxs/img/prikol/images/large/1/2/308321_879389.jpg', followed: true, fullName: 'Nastya', status: 'I am a good girl', location: { city: 'Charkov', country: 'Ukraine' } },
-        ]
-        )
+
+    let getUsers = () => {
+
+
+        if (props.users.length === 0) {
+
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            });
+        }
+
     }
 
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto} />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
                     </div>
                     <div>
                         {u.followed
-                            ? <button onClick={() => { props.unfollow(u.id) } } >Unfollow</button >
-                            : <button onClick={() => { props.follow(u.id) } } >Follow</button>}
+                            ? <button onClick={() => { props.unfollow(u.id) }} >Unfollow</button >
+                            : <button onClick={() => { props.follow(u.id) }} >Follow</button>}
 
                     </div>
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
