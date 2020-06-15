7b9e0903d3a72341f4230298aea5f3bd078f0a28
AlexeyZip <alexzipzap@gmail.com>
2020-06-15 17:04:44 +0300

    lesson 66

--- src/api/api.js
+++ src/api/api.js
@@ -5,27 +5,25 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers: {
-        "API-KEY": "a4dfae8ee-693d-4cc3-ab67-50e13d706c21",
+        "API-KEY": " add90d80-6378-4132-a247-c0a874835df3",
     }
+    // a4dfae8ee-693d-4cc3-ab67-50e13d706c21 
 });
 
 export const usersAPI = {
-    
+
     getUsers(currentPage = 1, pageSize = 100) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
             .then(response => {
                 return response.data
             });
-    }
+    },
+    follow(userId) {
+       return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+    },
+    unfollow(userId) {
+        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+    },
 }
-
-// export const usersFollowAPI = {
-//     getUsers(u){
-//         return instance.get(`follow/${u.id}`)
-//             .then(response => {
-//                 return response.data
-//             });
-//         }
-// }
 
 
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching, toggleFollowingProgress } from '../../redux/users-reducer';
+import { follow, unfollow,setCurrentPage,toggleFollowingProgress, getUsers } from '../../redux/users-reducer';
 import * as axios from 'axios';
 import Users from './Users';
 import Preloader from '../common/preloader/preloader';
@@ -8,35 +8,24 @@ import { usersAPI } from '../../api/api';
 
 class UsersContainer extends React.Component {
     componentDidMount() {
-        this.props.toggleIsFetching(true);
-        usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then(data => {
-            this.props.setUsers(data.items);
-            this.props.setTotalUsersCount(data.totalCount);
-            this.props.toggleIsFetching(false);
-        });
+        this.props.getUsers(this.props.currentPage, this.props.pageSize);
     }
 
     onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        this.props.toggleIsFetching(true);
-            usersAPI.getUsers(pageNumber, this.props.pageSize).then(data => {
-            this.props.setUsers(data.items);
-            this.props.toggleIsFetching(false);
-        });
+        this.props.getUsers(pageNumber, this.props.pageSize);
     }
     render() {
         return <>
             {this.props.isFetching ? <Preloader /> : null}
-            <Users totalUsersCount={this.props.totalUsersCount} 
-            pageSize={this.props.pageSize} 
-            currentPage={this.props.currentPage} 
-            onPageChanged={this.onPageChanged} 
-            users={this.props.users} 
-            follow={this.props.follow} 
-            unfollow={this.props.unfollow}
-            toggleFollowingProgress={this.props.toggleFollowingProgress}
-            followingInProgress={this.props.followingInProgress}
-             />
+            <Users totalUsersCount={this.props.totalUsersCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                onPageChanged={this.onPageChanged}
+                users={this.props.users}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+                followingInProgress={this.props.followingInProgress}
+            />
         </>
     }
 }
@@ -53,4 +42,5 @@ let mapStateToProps = (state) => {
 }
 
 
-export default connect(mapStateToProps,{follow,unfollow,setUsers,setCurrentPage,setTotalUsersCount,toggleIsFetching, toggleFollowingProgress})(UsersContainer);
+export default connect(mapStateToProps,
+    {follow, unfollow,setCurrentPage,toggleFollowingProgress, getUsers})(UsersContainer);
